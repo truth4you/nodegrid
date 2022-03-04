bd3457874fb55b0f583d849bbae8b81daf0921dc
webdev <liuzhemin0305@gmail.com>
2022-03-04 10:16:05 +0900

    token address fixed

--- pages/home.tsx
+++ pages/home.tsx
@@ -354,7 +354,7 @@ export default function Home() {
             <h2>Create a  Node with NodeGrid tokens to earn NodeGrid token rewards.</h2>
             <p>Rewards calculations are based on many factors, including the number of nodes, node revenue, token price, and protocol revenue, and they are variable.</p>
           </div>
-          <Link href="https://testnet.godex.exchange/swap?outputCurrency=0xE02Bde489389C9b115070AD2b994Eea7985d84B5" >
+          <Link href="https://testnet.godex.exchange/swap?outputCurrency=0x41512d704bef37902e2af615e5a6ecb5b13d7686" >
             <a target="_blank" className={classNames(styles.link,"w-full md:w-auto")}>Buy $TTK</a>
           </Link>
         </div>
